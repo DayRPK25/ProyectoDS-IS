d0d14ab8a4406b4ce756021535b79d1c9f176e6d
Javier184824 <leejavier62@gmail.com>
2026-05-16 13:59:12 -0600

    Cambiando direcciones para buscar archivos

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../config.php';
 $router = new Router();
 
 router->add('GET', '/', function () {
-    header('Content-Type: text/html; charset=utf-8');
     require_once __DIR__ . '/../views/home.php';
     exit;
 });
