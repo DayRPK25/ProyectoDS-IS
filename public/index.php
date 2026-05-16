2c4efc6c15f7de4c54818bcab457c1aafa3b8f85
Javier184824 <leejavier62@gmail.com>
2026-05-16 13:48:15 -0600

    Adding the home view

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+$router = new Router();
+
+router->add('GET', '/', function () {
+    header('Content-Type: text/html; charset=utf-8');
+    require_once __DIR__ . '/../views/home.php';
+    exit;
+});
+
+?>
