ca6e9335da17badedd5a77775ee63d39d410e834
Javier184824 <leejavier62@gmail.com>
2026-05-16 13:59:47 -0600

    Update home.php

--- views/home.php
+++ views/home.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acortador de URLs - LAMP MVC</title>
-    <link rel="stylesheet" href="/../public/style.css">
 </head>
 <body>
 
