54f3ba0dc22841f8d43d764fdaffb87ea59bc6fb
Javier184824 <leejavier62@gmail.com>
2026-05-16 21:12:43 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -71,7 +71,7 @@ $router = new Router();
 $router->add('GET', '/', function () {
     // Para el HTML desactivamos el Content-Type JSON que pusimos arriba
     header('Content-Type: text/html; charset=utf-8');
-    require_once __DIR__ . '/../views/home.html';
+    require_once __DIR__ . '/../views/login.html';
     exit;
 });
 
