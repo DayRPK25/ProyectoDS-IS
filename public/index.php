affff77ec04f1b331c64524c519aff9e4ba1a96f
Javier184824 <leejavier62@gmail.com>
2026-05-17 16:03:38 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -108,15 +108,16 @@ $router->add('POST', '/api/auth/login', function () {
 $router->add('POST', '/api/auth/register', function () {
     $response = [
         'idUsuario' => 0, // Hay que recuperar esto en la base de datos
-        'estado'    => 'registrado'
+        'estado'    => 'registrado',
+        'error'     => ''
     ];
     try {
         $data = json_decode(file_get_contents('php://input'), true);
         $pdo = Database::getInstance()->getConnection();
-        //query($pdo,
-        //    "INSERT INTO Usuario (correo, nombre, nombreUsuario, contrasena) VALUES (?, ?, ?, ?)",
-        //    [$data['correo'], $data['nombre'], $data['nombreUsuario'], password_hash($data['contrasena'], PASSWORD_BCRYPT)]
-        //);
+        query($pdo,
+            "INSERT INTO Usuario (correo, nombre, nombreUsuario, contrasena) VALUES (?, ?, ?, ?)",
+            [$data['correo'], $data['nombre'], $data['nombreUsuario'], password_hash($data['contrasena'], PASSWORD_BCRYPT)]
+        );
         http_response_code(200);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
@@ -125,6 +126,7 @@ $router->add('POST', '/api/auth/register', function () {
     }
     catch (Exception $e) {
         $response['estado'] = 'No registrado';
+        $response['error'] = $e;
         http_response_code(500);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
