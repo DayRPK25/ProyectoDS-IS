f46fc043ddcb49bf2f890fe46b11d797ef3c68d1
Javier184824 <leejavier62@gmail.com>
2026-05-17 16:11:22 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -108,31 +108,21 @@ $router->add('POST', '/api/auth/login', function () {
 $router->add('POST', '/api/auth/register', function () {
     $response = [
         'idUsuario' => 0, // Hay que recuperar esto en la base de datos
-        'estado'    => 'registrado',
-        'error'     => ''
+        'estado'    => 'registrado'
     ];
-    try {
-        $data = json_decode(file_get_contents('php://input'), true);
-        $pdo = Database::getInstance()->getConnection();
-        query($pdo,
-            "INSERT INTO Usuario (correo, nombre, nombreUsuario, contrasena) VALUES (?, ?, ?, ?)",
-            [$data['correo'], $data['nombre'], $data['nombreUsuario'], password_hash($data['contrasena'], PASSWORD_BCRYPT)]
-        );
+    
+    $data = json_decode(file_get_contents('php://input'), true);
+    $pdo = Database::getInstance()->getConnection();
+    query($pdo,
+        "INSERT INTO Usuario (correo, nombre, nombreUsuario, contrasena) VALUES (?, ?, ?, ?)",
+        [$data['correo'], $data['nombre'], $data['nombreUsuario'], $data['contrasena']]
+    );
 
-        http_response_code(200);
-        header('Content-Type: application/json; charset=utf-8');
-        echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    http_response_code(200);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
-        exit;
-    }
-    catch (Exception $e) {
-        $response['estado'] = 'No registrado';
-        $response['error'] = $e;
-        http_response_code(500);
-        header('Content-Type: application/json; charset=utf-8');
-        echo json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-        exit;
-    }
+    exit;
     
 });
 
